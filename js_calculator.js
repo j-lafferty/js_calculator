8891c41f57b06ea674d1f833d053aa807062a6d8
James Lafferty <james@j-lafferty.net>
2018-09-15 22:50:11 -0700

    Display digits and operators

--- js_calculator.js
+++ js_calculator.js
@@ -1,3 +1,9 @@
+var op;
+var num1;
+var num2;
+
+var x;
+
 function add() {
     return num1 + num2;
 };
@@ -30,4 +36,32 @@ function operator(op, num1, num2) {
             divide(num1, num2);
             break;
     };
+};
+//clear input
+function clear(button) {
+    x = button.value;
+    document.getElementById("display-scroll").innerHTML = x;
+};
+//change input number to negative or positive
+function plusmn(button) {
+
+};
+//convert input number to percentage
+function percent(button) {
+
+};
+
+function operation(button) {
+    x = button.value;
+    document.getElementById("display-scroll").innerHTML += x;
+};
+
+function digit(button) {
+    x = button.value;
+    document.getElementById("display-scroll").innerHTML += x;
+};
+
+function period(button) {
+    x = button.value;
+    document.getElementById("display-scroll").innerHTML += x;
 };
